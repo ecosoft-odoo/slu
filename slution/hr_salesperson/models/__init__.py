17217e04901719086a7b71f1dab1b033855bc917
newtratip <tharathip.chaweewongphan@gmail.com>
2020-04-11 01:58:19 +0700

    [13.0][UPD] hr_salesperson

--- slution/hr_salesperson/models/__init__.py
+++ slution/hr_salesperson/models/__init__.py
@@ -3,3 +3,4 @@
 from . import sale
 from . import res_partner
 from . import account_move
+from . import account_payment
--- slution/hr_salesperson/models/res_partner.py
+++ slution/hr_salesperson/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
@@ -15,6 +15,7 @@ class ResPartner(models.Model):
         domain=lambda self: self._get_domain_salesperson_id(),
     )
 
+    @api.model
     def _get_domain_salesperson_id(self):
         try:
             return [("department_id", "=", self.env.ref("hr.dep_sales").id)]
