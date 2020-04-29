aa4b436234e9ad84a4b84c858496710e63907ce5
newtratip <tharathip.chaweewongphan@gmail.com>
2020-04-29 19:16:03 +0700

    [13.0][ADD] script_transaction.sh

---
+++ slution/script/script_transaction.sh
@@ -0,0 +1,32 @@
+pg_user="odoo"
+prod_db="SLUTION_TEST"
+psql="psql -U $pg_user -d $prod_db"
+
+# Sales
+$psql -c "delete from sale_order_line";
+$psql -c "delete from sale_order";
+
+# Purchase
+$psql -c "delete from purchase_order_line"
+$psql -c "delete from purchase_order"
+
+# Stock
+$psql -c "delete from stock_move_line"
+$psql -c "delete from stock_move"
+$psql -c "delete from stock_picking"
+$psql -c "delete from stock_inventory"
+$psql -c "delete from stock_scrap"
+$psql -c "delete from stock_quant"
+$psql -c "delete from stock_valuation_layer"
+
+# Accounting
+$psql -c "delete from account_partial_reconcile";
+$psql -c "delete from account_move_line"
+$psql -c "delete from account_move"
+$psql -c "delete from account_payment"
+$psql -c "delete from account_check_deposit"
+$psql -c "delete from tax_adjustments_wizard";
+
+# Miscellaneous
+$psql -c "delete from mail_mail";
+$psql -c "delete from mail_message";
