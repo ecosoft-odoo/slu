45ff8333be62933e900d2095ea3c304efee4d80a
newtratip <tharathip.chaweewongphan@gmail.com>
2020-05-02 00:03:17 +0700

    [13.0][UPD] script_transaction.sh

--- slution/script/script_transaction.sh
+++ slution/script/script_transaction.sh
@@ -3,8 +3,8 @@ prod_db="SLUTION_TEST"
 psql="psql -U $pg_user -d $prod_db"
 
 # Sales
-$psql -c "delete from sale_order_line";
-$psql -c "delete from sale_order";
+$psql -c "delete from sale_order_line"
+$psql -c "delete from sale_order"
 
 # Purchase
 $psql -c "delete from purchase_order_line"
@@ -21,13 +21,14 @@ $psql -c "delete from stock_valuation_layer"
 $psql -c "delete from stock_production_lot"
 
 # Accounting
-$psql -c "delete from account_partial_reconcile";
+$psql -c "delete from account_partial_reconcile"
+$psql -c "delete from account_full_reconcile"
 $psql -c "delete from account_move_line"
 $psql -c "delete from account_move"
 $psql -c "delete from account_payment"
 $psql -c "delete from account_check_deposit"
-$psql -c "delete from tax_adjustments_wizard";
+$psql -c "delete from tax_adjustments_wizard"
 
 # Miscellaneous
-$psql -c "delete from mail_mail";
-$psql -c "delete from mail_message";
+$psql -c "delete from mail_mail"
+$psql -c "delete from mail_message"
