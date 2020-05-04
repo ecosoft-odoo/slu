b18f46c54d36f416cb87474122d21c11b6c942c3
newtratip <tharathip.chaweewongphan@gmail.com>
2020-05-04 19:33:05 +0700

    [13.0][UPD] Update salesperson_id in _create_invoice

--- slution/hr_salesperson/wizards/sale_make_invoice_advance.py
+++ slution/hr_salesperson/wizards/sale_make_invoice_advance.py
@@ -7,7 +7,9 @@ from odoo import models
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = "sale.advance.payment.inv"
 
-    def _prepare_invoice_values(self, order, name, amount, so_line):
-        res = super()._prepare_invoice_values(order, name, amount, so_line)
-        res["salesperson_id"] = order.salesperson_id.id
-        return res
+    def _create_invoice(self, order, so_line, amount):
+        invoice = super(SaleAdvancePaymentInv, self)._create_invoice(
+            order, so_line, amount
+        )
+        invoice.salesperson_id = order.salesperson_id.id
+        return invoice
